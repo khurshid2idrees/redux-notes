aac63af7322d708219111c95ab3812743b1b1b4a
khurshid2idrees <umarkhurshid3@gmail.com>
2024-05-10 10:25:54 +0530

    part:3

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
-// store
-
-const store = createStore(reducer, applyMiddleware(logger.default));
-
-
-// action name 
-
+// action name constants
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+const INCREMENTbyAMOUNT = "incrementByAmount";
 
 // reducer
 function reducer(state = { amount: 1 }, action) {
-  if (action.type === "increment") {
+  if (action.type === INCREMENT) {
     // immutability
     return { amount: state.amount + 1 };
   }
-  if (action.type === "decrement") {
+  if (action.type === DECREMENT) {
     return { amount: state.amount - 1 };
   }
-  if (action.type === "incrementByAmount") {
+  if (action.type === INCREMENTbyAMOUNT) {
     return { amount: state.amount + action.payload };
   }
   return state;
 }
 
-
+// store
+const store = createStore(reducer, applyMiddleware(logger.default));
 
 // Action creators
 
 function increment() {
-  return { type: "increment" };
-}
+  return { type: INCREMENT };
+} 
 
 function decrement() {
-  return { type: "decrement" };
+  return { type: DECREMENT };
 }
 
 function incrementByAmount(value) {
-  return { type: "incrementByAmount" , payload:value};
+  return { type: INCREMENTbyAMOUNT, payload: value };
 }
 
-
 setInterval(() => {
-store.dispatch(incrementByAmount(5));
+  store.dispatch(incrementByAmount(5));
 }, 2000);
-
 
 console.log(store.getState());
